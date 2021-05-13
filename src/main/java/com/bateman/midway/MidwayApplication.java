f6a6102b3741cb395b6784ad5a98fab1f41feeb1
Ethan Bateman <jetfire725@hotmail.com>
2021-05-13 19:30:01 -0400

    added code for editing server properties

--- src/main/java/com/bateman/midway/MidwayApplication.java
+++ src/main/java/com/bateman/midway/MidwayApplication.java
@@ -5,6 +5,7 @@ import java.io.File;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import com.bateman.midway.service.FileProcessor;
 import com.bateman.midway.service.SshClient;
 
 @SpringBootApplication
@@ -12,10 +13,12 @@ public class MidwayApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(MidwayApplication.class, args);
-			SshClient ssh = new SshClient();
-			File script = ssh.createScript();
-			ssh.copyScriptFile(script);
-			ssh.executePortForward();
+//			SshClient ssh = new SshClient();
+//			File script = ssh.createScript();
+//			ssh.copyScriptFile(script);
+//			ssh.executePortForward();
+		FileProcessor fp = new FileProcessor("src/main/resources");
+		fp.setServerProperty("server-ip", "123.456.678.345");
 	}
 
 }
--- src/main/java/com/bateman/midway/service/FileProcessor.java
+++ src/main/java/com/bateman/midway/service/FileProcessor.java
@@ -1,5 +1,60 @@
 package com.bateman.midway.service;
 
-public class FileProcessor {
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class FileProcessor {
+	
+	public FileProcessor(String path){
+		this.path = path;
+		properties = new File(path+"/server.properties");
+	}
+	String path;
+	File properties;
+	Logger log = LoggerFactory.getLogger(FileProcessor.class);
+	
+	//UPDATE PROPERTY IN SERVER.PROPERTIES FILE
+	public boolean setServerProperty(String property, String value) {
+		BufferedReader reader = null;;
+		FileWriter writer= null;;
+		try {
+			reader = new BufferedReader(new FileReader(properties));
+			String line = reader.readLine();
+			String data="";
+
+			while (line != null) {
+				if (line.startsWith(property)) {
+					line = property+"="+value;
+				}
+				data = data + line + System.lineSeparator();
+				line = reader.readLine();
+			}
+			reader.close();
+			writer = new FileWriter(properties);
+			writer.write(data);
+			writer.close();
+			return true;
+			
+		} catch (IOException e) {
+			log.error("ERROR UPDATING SERVER PROPERTY: "+e.getMessage());
+			return false;
+		} finally {
+			try {
+				if (reader != null) {
+					reader.close();
+				}
+				if (writer != null){
+					writer.close();
+				} 
+			} catch (Exception e) {
+				log.error("ERROR CLOSING READER, WRITER: "+e.getMessage());
+			}				
+		}
+	}	
 }
